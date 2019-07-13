03d430205c369b2e98332002b393a4ba890032a2
Sergey <sergey.u.severin@gmail.com>
2019-07-13 15:14:24 +0300

    factory

---
+++ hw27.php
@@ -0,0 +1,17 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+spl_autoload_register(function ($class) {
+    require_once 'Classes/' . $class . '.php';
+});
+
+$taxi = new Econom();
+$car = $taxi->callTaxi();
+print "Your type is : {$car->choose_type()}. Car : {$car->getCar()} . Price : {$car->getPrice()}" . "<br>";
+$taxi = new Standart();
+$car = $taxi->callTaxi();
+print "Your type is : {$car->choose_type()}. Car : {$car->getCar()} . Price : {$car->getPrice()}" . "<br>";
+$taxi = new Lux();
+$car = $taxi->callTaxi();
+print "Your type is : {$car->choose_type()}. Car : {$car->getCar()} . Price : {$car->getPrice()}";
